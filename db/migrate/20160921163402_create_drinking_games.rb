24636b46fa519ad29e304ae4e8f12eac70e56a33
Michael Haney <michael.haney@colorado.edu>
2016-09-21 11:12:21 -0600

    new migration w/ bootstrap

--- db/migrate/20160921163402_create_drinking_games.rb
+++ db/migrate/20160921163402_create_drinking_games.rb
@@ -1,6 +1,5 @@
 class CreateDrinkingGames < ActiveRecord::Migration
   def change
-    drop_table :drinking_games
     create_table :drinking_games do |t|
       t.string :name
       t.string :number_of_people
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915003742) do
+ActiveRecord::Schema.define(version: 20160921163402) do
 
   create_table "drinking_games", force: :cascade do |t|
     t.string   "name"
